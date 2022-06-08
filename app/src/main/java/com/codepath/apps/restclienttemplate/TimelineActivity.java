61b409d56613b356a4debab81f7252be626a31b9
Shaniya Clement <shaniyaclement@fb.com>
2022-06-08 16:42:08 -0700

    Application after adding a Menu Item in the ActionBar that will be used to allow the user to compose a tweet. Overrode 'onOptionsItemSelected' and 'onCreateOptionsMenu', inflated the menu main layout, created a toast to signify functionality for now.

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -1,5 +1,6 @@
 package com.codepath.apps.restclienttemplate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -7,8 +8,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
@@ -49,6 +53,24 @@ public class TimelineActivity extends AppCompatActivity {
         populateHomeTimeline();
         //onLogoutButton();
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Inflate the menu: adds items to action bar if it is present
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.compose) {
+            // Compose icon has been selected
+            Toast.makeText(this, "Compose!", Toast.LENGTH_SHORT).show();
+            //Navigate to the compose activity
+        }
+        // We want to consume the tap of the menu item so we will return true
+        return true;
     }
 
     private void populateHomeTimeline() {
